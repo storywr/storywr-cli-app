a5134c14e2b07fa848723acf88137c1acb2d3555
William R. Story <storywr@gmail.com>
2016-10-27 18:19:32 -0400

    get games

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,14 +1,11 @@
 class Cli
 
 	def call
-		loop do
-			puts "Welcome to NFL Helper!"
-			puts "Enter 'list' to list games for the current week"
-			input = gets.strip
-				if input == "list"
-					list_games
-					break
-				end
+		puts "Welcome to NFL Helper!"
+		puts "Enter 'list' to list games for the current week"
+		input = gets.strip
+		if input == "list"
+			list_games
 		end
 	end
 
@@ -17,10 +14,11 @@ class Cli
 		puts "NFL Games this Week:"
 		puts "Would you like to check ticket prices? (y/n)"
 		input = gets.strip
-			if input == "y"
-				puts "Enter the number of the game you would like to see"
-				input = gets.strip
-			end
+		if input == "y"
+			puts "Enter the corresponding number of the game you would like to see"
+			input = gets.strip.to_i
+			Scraper.tickets(input)
+		end
 	end
 
 end
--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,11 +1,18 @@
+require 'nokogiri'
+require 'open-uri'
+
 class Scraper
 
-	def initialize
+	def get_games
+		doc = Nokogiri::HTML(http://www.espn.com/nfl/schedule)
+		scraped_games = []
+		away = doc.css(".away .team-name")
+		home = doc.css(".home .team-name")
+		scraped_games << "#{away} at #{home}"
 	end
 
-
-
-
+	def tickets(game_number)
+	end
 
 
 
