14cbcf962d68a45c7bb46671c05337dce4017b4a
William R. Story <storywr@gmail.com>
2016-10-28 01:28:10 -0400

    narrowed in css

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,9 +6,15 @@ class Scraper
 	def get_games
 		doc = Nokogiri::HTML(http://www.espn.com/nfl/schedule)
 		scraped_games = []
-		away = doc.css(".away .team-name")
-		home = doc.css(".home .team-name")
-		scraped_games << "#{away} at #{home}"
+		counter = 0
+		games = doc.css(".schedule has-team-logos align-left tr")
+		games.each do |game|
+			away = game.css("td a .team-name").text
+			home = game.css("td a .home .team-name").text
+			away.sub("#{home}", "")
+			scraped_games << "#{counter}. #{away} at #{home}"
+			counter += 1
+		end
 	end
 
 	def tickets(game_number)
