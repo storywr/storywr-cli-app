feec8e4e2336977a646c45e14222b21d32477bcd
William R. Story <storywr@gmail.com>
2016-10-31 17:14:05 -0400

    adjusted css

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -14,6 +14,7 @@ class Scraper
 		games.each do |game|
 			away = game.css(".team-name").text
 			home = game.css(".home .team-name").text
+			away = away.sub("#{home}", "")
 			@scraped_games << "#{counter}. #{away} at #{home}"
 			@tickets[counter.to_s.to_sym] = game.css("href")
 			counter += 1
