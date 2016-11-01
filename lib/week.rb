eb05bbd350b4bbbaa73bd9b8fc6134da34feeb28
William R. Story <storywr@gmail.com>
2016-11-01 16:14:55 -0400

    added to objects

--- lib/week.rb
+++ lib/week.rb
@@ -1,12 +1,22 @@
 class Week
-  attr_accessor :all
+  attr_accessor :number, :team
+
   @@all = []
 
   def initialize(number)
+    @number = number
   end
 
   def self.all
     @@all
+  end
+
+  def save
+    @@all << self
+  end
+
+  def add_team(team)
+    @teams << team
   end
 
 end
