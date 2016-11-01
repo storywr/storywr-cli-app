2c53b43d59cabc914f68a8eccdc511a654a94394
William R. Story <storywr@gmail.com>
2016-11-01 00:28:12 -0400

    added stats

--- lib/cli.rb
+++ lib/cli.rb
@@ -6,21 +6,43 @@ class CLI
 
 
 	def start
-		puts "Welcome to NFL Ticket Finder!"
-		puts "Enter NFL Week Number"
-		input = gets.strip.to_s
-		puts ""
-		puts "NFL Games this Week:"
-		Scraper.get_games(input)
-		Scraper.show_games
-		puts "Would you like to check ticket prices? (y/n)"
+		puts
+		puts "Welcome to NFL Stats and Ticket Finder!"
+		puts "Enter 'Stats' to see NFL scores and stat lines and 'Tickets' to find Tickets for upcoming games."
 		input = gets.strip
-		if input == "y"
-			puts ""
-			puts "Enter corresponding game number or team."
-			input = gets.strip
-			Scraper.ticket_prices(input)
+		if input == "Stats"
+			find_stats
+		elsif input == "Tickets"
+			find_tickets
 		end
+	end
+
+	def find_stats
+		puts
+		puts "Enter NFL Week Number"
+		week = gets.strip.to_s
+		display_games(week)
+		puts "Enter corresponding game number or team name."
+		#input = gets.strip
+		#Scraper.game_stats(input)
+	end
+
+	def find_tickets
+		puts
+		puts "Enter NFL Week Number"
+		week = gets.strip.to_s
+		display_games(week)
+		puts
+		puts "Enter corresponding game number or team name."
+		input = gets.strip
+		Scraper.ticket_prices(input)
+	end
+
+	def display_games(week)
+		puts
+		puts "NFL Games Week #{week}:"
+		Scraper.get_games(week)
+		Scraper.show_games
 	end
 
 end
